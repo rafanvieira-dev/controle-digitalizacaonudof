50b3d5a541762c8c854d354c22d14057e467804a
rafanvieira-dev <rafan.vieira@gmail.com>
2026-02-26 09:16:28 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDs1mj1e6SKK-qwa_wXLGYI3CIitsO6SNk",
+  authDomain: "controledigitalizacaodoc.firebaseapp.com",
+  projectId: "controledigitalizacaodoc",
+  storageBucket: "controledigitalizacaodoc.firebasestorage.app",
+  messagingSenderId: "1057595169300",
+  appId: "1:1057595169300:web:86c34690b376b2649da501"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
