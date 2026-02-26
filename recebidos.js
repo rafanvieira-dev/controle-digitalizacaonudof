e9dc0f30772651c758055ef8074d9fa59d938b8d
rafanvieira-dev <rafan.vieira@gmail.com>
2026-02-26 09:27:51 -0300

    Create recebidos.js

---
+++ recebidos.js
@@ -0,0 +1,60 @@
+import { db, auth } from "./firebase.js";
+import { 
+  collection, 
+  getDocs, 
+  updateDoc, 
+  doc 
+} from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
+
+import { onAuthStateChanged } 
+from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+  if (!user) window.location.href = "index.html";
+  else carregar();
+});
+
+async function carregar() {
+
+  const lista = document.getElementById("listaRecebidas");
+  lista.innerHTML = "";
+
+  const snapshot = await getDocs(collection(db, "guias"));
+
+  snapshot.forEach(docSnap => {
+    const guia = docSnap.data();
+
+    if (guia.status === "Ativa") {
+
+      lista.innerHTML += `
+        <div style="margin-bottom:20px;">
+          <strong>Guia Nº ${guia.numeroGuia}</strong>
+          <button onclick="arquivar('${docSnap.id}')">Arquivar</button>
+        </div>
+      `;
+    }
+  });
+}
+
+window.arquivar = async function(idGuia) {
+
+  const dataArquivamento = prompt("Data do Arquivamento:");
+  const caixaGuia = prompt("Nº da Caixa Localização Guia:");
+  const observacao = prompt("Observação:");
+
+  await updateDoc(doc(db, "guias", idGuia), {
+    status: "Arquivada",
+    arquivamento: {
+      dataArquivamento,
+      caixaGuia,
+      observacao
+    }
+  });
+
+  alert("Guia arquivada com sucesso!");
+  carregar();
+};
+
+window.voltar = function() {
+  window.location.href = "index.html";
+};
